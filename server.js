278b3bd734060f5ae48a6769fb8466a3b67116a6
HUNGRYCAKES\hungry <noemail@noemail.com>
2023-10-22 19:51:12 -0400

    no message

--- server.js
+++ server.js
@@ -54,11 +54,11 @@ wss.on('connection', function connection (client) {
 					if (client.id == player.id) {
 						var paramsArray = [`${player.id}`, `${player.socketsid}`];
 						var paramJson = JSON.stringify(paramsArray);
-						aClient.send(`{"Classname": "GameManager", "Methodname": "InitPlayersWSS", "Parameters": ${paramJson} }`);
+						aClient.send(`{"Classname": "GameManager", "Methodname": "InitPlayersWSS", "Parameters": "${paramJson}" }`);
 					} else {
 						var paramsArray = [`${player.id}`, `${emptyString}`];
 						var paramJson = JSON.stringify(paramsArray);
-						aClient.send(`{"Classname": "GameManager", "Methodname": "InitPlayersWSS", "Parameters": ${paramJson} }`);
+						aClient.send(`{"Classname": "GameManager", "Methodname": "InitPlayersWSS", "Parameters": "${paramJson}" }`);
 					}
 				  
 				});
